c5fcff25877579cdb1aafcbbf780e85b00621efb
kamakshi1321 <159157322+kamakshi1321@users.noreply.github.com>
2024-02-08 20:50:19 +0530

    dict_tuples.py

---
+++ dict_tuples.py
@@ -0,0 +1,13 @@
+#working with dictionaries
+marks={"chemistry":90, "maths": 96, "english": 78}
+print(marks["english"])
+marks["physics"]=45
+print(marks)
+marks["english"]=80
+print(marks)
+
+#working with tuples
+vals=array('u',['c','v','l'])
+for u in vals:
+    print(u)
+    print(vals)
